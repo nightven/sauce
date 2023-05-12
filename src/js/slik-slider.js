cf4232bbebb024cf184f233ba42c7754993adc7d
ExLuZiVe53 <imstudent27@gmail.com>
2023-05-12 09:57:26 +0200

    invaite slick-slider.js

---
+++ src/js/slik-slider.js
@@ -0,0 +1,22 @@
+$('.slider').slick({
+  mobileFirst: true
+});
+
+
+// $('.offer').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   arrows: false,
+//   dots: true,
+//   fade: true,
+//   asNavFor: '.slider-photo'
+// });
+// $('.slider-photo').slick({
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   asNavFor: '.offer',
+//   arrows: false,
+//   fade: true,
+//   centerMode: true,
+//   focusOnSelect: true
+// });
